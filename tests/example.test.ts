d2787d697bdba399236ae4d496385b8b58d64efe
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-02-25 00:31:09 +0100

    Add a biome action

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 describe("test suite", () => {
 	it("works", () => {
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,14 +2,10 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    coverage: {
-      provider: "v8",
-	  exclude: [
-		  'node_modules',
-		  'dist',
-		  'docs'
-	  ]
-    },
-  },
+	test: {
+		coverage: {
+			provider: "v8",
+			exclude: ["node_modules", "dist", "docs"],
+		},
+	},
 });
